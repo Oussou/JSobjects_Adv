9e27196eb83c00fe88cc21b28329ff6c2fe0aa4f
oussou <ousmanetoure@claremontihs.org>
2016-12-01 23:16:51 +0000

    whatever

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,13 +1,16 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
+/*global library*/
+/*global $*/
+console.log(library.music.Micheal_Jackson.year)
 
 
 //Task 2: Console the name of the song Lady Gaga released in 2006 
 
+console.log(library.music.Lady_Gaga.song)
 
-
-$( document ).ready(function() {
+$( document).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    
+  
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
 
 });
